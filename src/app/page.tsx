479d7841e4d27def9470d3b5a976b57851fc464c
dev-064 <divyanshujha112@gmail.com>
2024-01-31 11:25:15 +0530

    initialised the basic next app and installed the needed packages for frontend and backend

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="h-screen w-screen flex justify-center items-center flex-col">
+      <p className="text-6xl font-bold">Black Rabbit</p>
+      <p className="text-3xl font-semibold">Blog Posting/Viewing</p>
+    </main>
+  );
+}
