1a54a020bd9ea5e7cf85dd45afcb8f989080054c
Vineet Jakhar <100095098+4vinn@users.noreply.github.com>
2024-02-18 14:50:47 +0530

    added report filters and few design changes

--- src/app/(client)/blogs/page.tsx
+++ src/app/(client)/blogs/page.tsx
@@ -1,7 +1,25 @@
 "use client";
 import Card from "@/components/Card";
+import { useState,useEffect } from "react";
+import axios from "axios";
+
 
 export default function Reports() {
+  const [blogs, SetBlogs] = useState<any[]>([]);
+  useEffect(() => {
+    axios.post('/api/blog', {
+      is_report: false,
+    }).then((response) => {
+      if (response.data.code === 0) {
+        console.log(response.data.code);
+        SetBlogs(response.data.data);
+      }
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, [])
+
+
   return (
     <div>
       <div className="flex  flex-col w-[84vw] m-auto ">
@@ -15,62 +33,70 @@ export default function Reports() {
             tag="Technology"
             title="Blog 1"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            slug="ldkjfldkfj"
           />
           <Card
             imageUrl="https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             author="John Doe"
             date="February 3, 2024"
             tag="Technology"
-            title="Blog 2"
+            title="Blog 1"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            slug="ldkjfldkfj"
           />
           <Card
             imageUrl="https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             author="John Doe"
             date="February 3, 2024"
             tag="Technology"
-            title="Blog 3"
+            title="Blog 1"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            slug="ldkjfldkfj"
           />
           <Card
             imageUrl="https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             author="John Doe"
             date="February 3, 2024"
             tag="Technology"
-            title="Blog 4"
+            title="Blog 1"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            slug="ldkjfldkfj"
           />
           <Card
             imageUrl="https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             author="John Doe"
             date="February 3, 2024"
             tag="Technology"
-            title="Blog 5"
+            title="Blog 1"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            slug="ldkjfldkfj"
           />
           <Card
             imageUrl="https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             author="John Doe"
             date="February 3, 2024"
             tag="Technology"
-            title="Blog 6"
+            title="Blog 1"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            slug="ldkjfldkfj"
           />
           <Card
             imageUrl="https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             author="John Doe"
             date="February 3, 2024"
             tag="Technology"
-            title="Blog 7"
+            title="Blog 1"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            slug="ldkjfldkfj"
           />
           <Card
             imageUrl="https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             author="John Doe"
             date="February 3, 2024"
             tag="Technology"
-            title="Blog 8"
+            title="Blog 1"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            slug="ldkjfldkfj"
           />
         </div>
       </div>
